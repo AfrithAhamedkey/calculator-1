b1856097feb5bc817ec568e64346cbafbb2c01d0
AfrithAhamedkey <afrithniyas@gmail.com>
2021-08-08 16:33:58 +0530

    code1forcalculator

--- src/java/com/kelaniya/unit/helloworld.java
+++ src/java/com/kelaniya/unit/helloworld.java
@@ -4,6 +4,21 @@ public class helloworld {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        Scanner scanner = new (System.in);
+        System.out.println("Enter the first number");
+
+        double firstnum = scanner.nextdouble();
+
+        System.out.println("Enter the second number");
+        double secondnum = scanner.nextdouble();
+
+        double answer = firstnum + secondnum ;
+        System.out.println("sum is =" + answer);
+
+
+
+
     }
 
 }
